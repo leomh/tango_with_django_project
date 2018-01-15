3fce58bb73ffcc3b44b74ea18bca3df1139934b4
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-01-15 18:22:19 +0000

    ch5: models defined

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# all models have a default id field that acts as a primary key
+
+class Category(models.Model):
+    # store character data
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    # store category, title, url for the page
+    # Page has a one-to-many relationships with model Category
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
