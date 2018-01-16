d65b04268690215e147ebc56293e5e1ce14b20c2
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-01-16 00:17:11 +0000

    ch5: populate_rango script

--- rango/models.py
+++ rango/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Category(models.Model):
     # store character data
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
-    def __self__(self):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
