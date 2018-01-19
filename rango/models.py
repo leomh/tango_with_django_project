ef4bf03579d7fc4c6a6e31b39ed5d76a1311d4f7
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-01-19 15:25:43 +0000

    ch6: url design and mapping with slugify

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # all models have a default id field that acts as a primary key
 
@@ -7,6 +8,12 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        # replace whitespace in urls with hyphens
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
