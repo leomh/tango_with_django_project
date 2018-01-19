b6f6a8c76002d71fbe40f67aca7a952e5e723bb0
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-01-19 14:53:55 +0000

    ch5: updated index view to display categories

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+
 
 def index(request):
-	# dictionary will be passed to the template engine as its context
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	# the render takes the user request, the index page, and the dictionary to produce an HTML page
-	# which is then dispatched to the user's web browser
-	return render(request, 'rango/index.html', context=context_dict)
-	
+    # Query the database for a list of all categories  currently stored
+    # Order the categories by no. likes in descending order
+    # Retrieve the top 5 only - or all if less than 5
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    # Render the response
+    return render(request, 'rango/index.html', context_dict)
+
+
 def about(request):
-	return render(request, 'rango/about.html')
-	# below removed after completing chapter 4 exercise
-	# return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
+    # below removed after completing chapter 4 exercise
+    # return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
