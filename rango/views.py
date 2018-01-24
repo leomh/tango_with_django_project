86fe511de8448aa0ed817041c9c9730ce57595dc
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-01-24 19:07:04 +0000

    ch6: index view now displays most viewed categories and pages

--- rango/views.py
+++ rango/views.py
@@ -13,9 +13,9 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     # Same thing for pages
 
-    pages_list = Page.objects.order_by('-views')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {'categories': category_list, 'pages': pages_list}
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     # Render the response
     return render(request, 'rango/index.html', context_dict)
