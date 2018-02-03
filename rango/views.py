515c29cb87bf5a1acdb1acba000e4892a3444866
Leonore Papaloizos <2264897v@student.gla.ac.uk>
2018-02-03 13:56:04 +0000

    ch9: added login functonalities and restrictions

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -54,7 +58,7 @@ def show_category(request, category_name_slug):
     # Render the response and return it
     return render(request, 'rango/category.html', context_dict)
 
-
+@login_required
 def add_category(request):
     # three scenarios:
     # showing a new blank form for adding a category
@@ -84,7 +88,7 @@ def add_category(request):
     # Render the form with any error messages
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -142,3 +146,35 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render(request, 'rango/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        # get() will return None if the value does not exist instead of a KeyError exception
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html', {})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
